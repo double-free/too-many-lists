95d4114cb0eca27b9d1f2cbbb46e48a192b0e080
Ye Yuan <yuanye_ptr@qq.com>
2022-09-15 00:14:00 +0800

    add pop_front method

--- src/fourth.rs
+++ src/fourth.rs
@@ -10,6 +10,8 @@
 //      Rc<RefCell<T>> to reintroduce mutability
 //   4. Only two operation matters in RefCell, borrow() and borrow_mut()
 //   5. RefCell<T> is only for single-threaded scenarios, use Mutex<T> for multi-threaded situation
+//   6. Rc<T> can't release if it holds strong ref to each other,
+//      so we need to manually pop elements in Drop
 
 use std::cell::RefCell;
 use std::rc::Rc;
@@ -22,7 +24,7 @@ pub struct List<T> {
 }
 
 struct Node<T> {
-    value: T,
+    elem: T,
     next: Link<T>,
     prev: Link<T>,
 }
@@ -49,14 +51,70 @@ impl<T> List<T> {
             }
         }
     }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        return self.head.take().map(|old_head| {
+            match old_head.borrow_mut().next.take() {
+                None => {
+                    // reset tail
+                    self.tail = None;
+                }
+                Some(new_head) => {
+                    new_head.borrow_mut().prev = None;
+                    self.head = Some(new_head);
+                }
+            }
+            return Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem;
+        });
+    }
 }
 
 impl<T> Node<T> {
     pub fn new(elem: T) -> Rc<RefCell<Self>> {
         Rc::new(RefCell::new(Node {
-            value: elem,
+            elem: elem,
             prev: None,
             next: None,
         }))
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        while self.pop_front().is_some() {}
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop_front(), None);
+
+        // Populate list
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+
+        // Check normal removal
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push_front(4);
+        list.push_front(5);
+
+        // Check normal removal
+        assert_eq!(list.pop_front(), Some(5));
+        assert_eq!(list.pop_front(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), None);
     }
 }
