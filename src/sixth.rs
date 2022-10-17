dea2ed288ce84b9bfddc9f87bc0f223a36bc18a1
Ye Yuan <yuanye_ptr@qq.com>
2022-10-17 23:59:48 +0800

    pass all tests

--- src/sixth.rs
+++ src/sixth.rs
@@ -61,35 +61,73 @@ impl<T> LinkedList<T> {
         self.len += 1;
     }
 
-    pub fn pop_front(&mut self) -> Option<T> {
+    pub fn push_back(&mut self, elem: T) {
         unsafe {
-            // Only have to do stuff if there is a front node to pop.
-            // Note that we don't need to mess around with `take` anymore
-            // because everything is Copy and there are no dtors that will
-            // run if we mess up... right? :) Riiiight? :)))
-            self.front.map(|node| {
-                let boxed_node = Box::from_raw(node.as_ptr());
-                let result = boxed_node.elem;
+            let new_tail = std::ptr::NonNull::new_unchecked(Box::into_raw(Box::new(Node {
+                prev: None,
+                next: None,
+                elem: elem,
+            })));
 
-                self.front = boxed_node.next;
+            if let Some(old_tail) = self.back {
+                (*old_tail.as_ptr()).next = Some(new_tail);
+                (*new_tail.as_ptr()).prev = Some(old_tail);
+            } else {
+                // no existing element, the new one is both the front and back
+                self.front = Some(new_tail);
+            }
 
-                match self.front {
-                    Some(new_head) => {
-                        // Cleanup its reference to the removed node
-                        (*new_head.as_ptr()).prev = None;
-                    }
-                    None => {
-                        // If the front is now null, then this list is now empty!
-                        debug_assert!(self.len == 1);
-                        self.back = None;
-                    }
-                }
-
-                self.len -= 1;
-
-                return result;
-            })
+            self.back = Some(new_tail);
         }
+        self.len += 1;
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        // Only have to do stuff if there is a front node to pop.
+        // Note that we don't need to mess around with `take` anymore
+        // because everything is Copy and there are no dtors that will
+        // run if we mess up... right? :) Riiiight? :)))
+        self.front.map(|node| unsafe {
+            let boxed_node = Box::from_raw(node.as_ptr());
+            let result = boxed_node.elem;
+
+            self.front = boxed_node.next;
+
+            match self.front {
+                Some(new_head) => {
+                    // Cleanup its reference to the removed node
+                    (*new_head.as_ptr()).prev = None;
+                }
+                None => {
+                    // If the front is now null, then this list is now empty!
+                    debug_assert!(self.len == 1);
+                    self.back = None;
+                }
+            }
+
+            self.len -= 1;
+
+            return result;
+        })
+    }
+
+    pub fn pop_back(&mut self) -> Option<T> {
+        self.back.map(|node_ptr| unsafe {
+            let boxed_node = Box::from_raw(node_ptr.as_ptr());
+            // moved value out
+            let result = boxed_node.elem;
+
+            self.back = boxed_node.prev;
+            if let Some(new_tail) = self.back {
+                (*new_tail.as_ptr()).next = None;
+            } else {
+                // last element is popped
+                self.front = None;
+            }
+
+            self.len -= 1;
+            return result;
+        })
     }
 
     pub fn len(&self) -> usize {
@@ -103,6 +141,14 @@ impl<T> LinkedList<T> {
     pub fn front_mut(&mut self) -> Option<&mut T> {
         self.front
             .map(|mut node| unsafe { &mut node.as_mut().elem })
+    }
+
+    pub fn back(&self) -> Option<&T> {
+        self.back.map(|node| unsafe { &node.as_ref().elem })
+    }
+
+    pub fn back_mut(&mut self) -> Option<&mut T> {
+        self.back.map(|mut node| unsafe { &mut node.as_mut().elem })
     }
 }
 
@@ -127,10 +173,28 @@ impl<'a, T> Iterator for Iter<'a, T> {
             return None;
         }
 
+        self.len -= 1;
         return self.front.map(|node| unsafe {
-            self.len -= 1;
             self.front = (*node.as_ptr()).next;
-            &(*node.as_ptr()).elem
+            &node.as_ref().elem
+        });
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.len, Some(self.len))
+    }
+}
+
+impl<'a, T> DoubleEndedIterator for Iter<'a, T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.len == 0 {
+            return None;
+        }
+
+        self.len -= 1;
+        return self.back.map(|node| unsafe {
+            self.back = (*node.as_ptr()).prev;
+            &node.as_ref().elem
         });
     }
 }
@@ -161,10 +225,28 @@ impl<'a, T> Iterator for IterMut<'a, T> {
             return None;
         }
 
-        return self.front.map(|node| unsafe {
+        return self.front.map(|mut node| unsafe {
             self.len -= 1;
-            self.front = (*node.as_ptr()).next;
-            &mut (*node.as_ptr()).elem
+            self.front = node.as_ref().next;
+            &mut node.as_mut().elem
+        });
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.len, Some(self.len))
+    }
+}
+
+impl<'a, T> DoubleEndedIterator for IterMut<'a, T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.len == 0 {
+            return None;
+        }
+
+        self.len -= 1;
+        return self.back.map(|mut node| unsafe {
+            self.back = node.as_ref().prev;
+            &mut node.as_mut().elem
         });
     }
 }
@@ -188,6 +270,10 @@ impl<T> Iterator for IntoIter<T> {
     fn next(&mut self) -> Option<Self::Item> {
         self.0.pop_front()
     }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.0.len, Some(self.0.len))
+    }
 }
 
 impl<T> LinkedList<T> {
@@ -196,9 +282,91 @@ impl<T> LinkedList<T> {
     }
 }
 
+impl<T> DoubleEndedIterator for IntoIter<T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.0.pop_back()
+    }
+}
+
+//////////////////////
+// other interfaces //
+//////////////////////
+
+impl<T: std::hash::Hash> std::hash::Hash for LinkedList<T> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.len().hash(state);
+        for item in self.iter() {
+            item.hash(state);
+        }
+    }
+}
+
+impl<T: std::fmt::Debug> std::fmt::Debug for LinkedList<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_list().entries(self.iter()).finish()
+    }
+}
+
+impl<T: Clone> Clone for LinkedList<T> {
+    fn clone(&self) -> Self {
+        let mut new_list = Self::new();
+        for item in self.iter() {
+            new_list.push_back(item.clone());
+        }
+        new_list
+    }
+}
+
+impl<T> Extend<T> for LinkedList<T> {
+    fn extend<I: IntoIterator<Item = T>>(&mut self, iter: I) {
+        for item in iter {
+            self.push_back(item);
+        }
+    }
+}
+
+impl<T> FromIterator<T> for LinkedList<T> {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let mut list = Self::new();
+        list.extend(iter);
+        list
+    }
+}
+
+// if T implements PartialEq, so does LinkedList<T>
+impl<T: PartialEq> PartialEq for LinkedList<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.len() == other.len() && self.iter().eq(other.iter())
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        self.eq(other) == false
+    }
+}
+impl<T: Eq> Eq for LinkedList<T> {}
+
+impl<T: PartialOrd> PartialOrd for LinkedList<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.iter().partial_cmp(other.iter())
+    }
+}
+impl<T: Ord> Ord for LinkedList<T> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.iter().cmp(other.iter())
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::LinkedList;
+
+    fn generate_test() -> LinkedList<i32> {
+        list_from(&[0, 1, 2, 3, 4, 5, 6])
+    }
+
+    fn list_from<T: Clone>(v: &[T]) -> LinkedList<T> {
+        v.iter().map(|x| (*x).clone()).collect()
+    }
 
     #[test]
     fn test_basic_front() {
@@ -241,53 +409,222 @@ mod test {
     }
 
     #[test]
-    fn into_iter() {
-        let mut list = LinkedList::new();
-        list.push_front(1);
-        list.push_front(2);
-        list.push_front(3);
+    fn test_basic() {
+        let mut m = LinkedList::new();
+        assert_eq!(m.pop_front(), None);
+        assert_eq!(m.pop_back(), None);
+        assert_eq!(m.pop_front(), None);
+        m.push_front(1);
+        assert_eq!(m.pop_front(), Some(1));
+        m.push_back(2);
+        m.push_back(3);
+        assert_eq!(m.len(), 2);
+        assert_eq!(m.pop_front(), Some(2));
+        assert_eq!(m.pop_front(), Some(3));
+        assert_eq!(m.len(), 0);
+        assert_eq!(m.pop_front(), None);
+        m.push_back(1);
+        m.push_back(3);
+        m.push_back(5);
+        m.push_back(7);
+        assert_eq!(m.pop_front(), Some(1));
 
-        let mut iter = list.into_iter();
-        assert_eq!(iter.next(), Some(3));
-        assert_eq!(iter.next(), Some(2));
-        assert_eq!(iter.next(), Some(1));
-        assert_eq!(iter.next(), None);
-    }
-
-    #[test]
-    fn iter() {
-        let mut list = LinkedList::new();
-        list.push_front(1);
-        list.push_front(2);
-        list.push_front(3);
-
-        let mut iter = list.iter();
-        assert_eq!(iter.next(), Some(&3));
-        assert_eq!(iter.next(), Some(&2));
-        assert_eq!(iter.next(), Some(&1));
-
-        let mut iter1 = list.iter();
-        assert_eq!(iter1.next(), Some(&3));
-    }
-
-    #[test]
-    fn iter_mut() {
-        let mut list = LinkedList::new();
-        list.push_front(1);
-        list.push_front(2);
-        list.push_front(3);
-
-        let mut iter = list.iter_mut();
-        assert_eq!(iter.next(), Some(&mut 3));
-        assert_eq!(iter.next(), Some(&mut 2));
-        assert_eq!(iter.next(), Some(&mut 1));
-
-        // modify
-        for value in list.iter_mut() {
-            *value *= 2;
+        let mut n = LinkedList::new();
+        n.push_front(2);
+        n.push_front(3);
+        {
+            assert_eq!(n.front().unwrap(), &3);
+            let x = n.front_mut().unwrap();
+            assert_eq!(*x, 3);
+            *x = 0;
         }
+        {
+            assert_eq!(n.back().unwrap(), &2);
+            let y = n.back_mut().unwrap();
+            assert_eq!(*y, 2);
+            *y = 1;
+        }
+        assert_eq!(n.pop_front(), Some(0));
+        assert_eq!(n.pop_front(), Some(1));
+    }
 
-        let mut iter1 = list.iter_mut();
-        assert_eq!(iter1.next(), Some(&mut 6));
+    #[test]
+    fn test_iterator() {
+        let m = generate_test();
+        for (i, elt) in m.iter().enumerate() {
+            assert_eq!(i as i32, *elt);
+        }
+        let mut n = LinkedList::new();
+        assert_eq!(n.iter().next(), None);
+        n.push_front(4);
+        let mut it = n.iter();
+        assert_eq!(it.size_hint(), (1, Some(1)));
+        assert_eq!(it.next().unwrap(), &4);
+        assert_eq!(it.size_hint(), (0, Some(0)));
+        assert_eq!(it.next(), None);
+    }
+
+    #[test]
+    fn test_iterator_double_end() {
+        let mut n = LinkedList::new();
+        assert_eq!(n.iter().next(), None);
+        n.push_front(4);
+        n.push_front(5);
+        n.push_front(6);
+        let mut it = n.iter();
+        assert_eq!(it.size_hint(), (3, Some(3)));
+        assert_eq!(it.next().unwrap(), &6);
+        assert_eq!(it.size_hint(), (2, Some(2)));
+        assert_eq!(it.next_back().unwrap(), &4);
+        assert_eq!(it.size_hint(), (1, Some(1)));
+        assert_eq!(it.next_back().unwrap(), &5);
+        assert_eq!(it.next_back(), None);
+        assert_eq!(it.next(), None);
+    }
+
+    #[test]
+    fn test_rev_iter() {
+        let m = generate_test();
+        for (i, elt) in m.iter().rev().enumerate() {
+            assert_eq!(6 - i as i32, *elt);
+        }
+        let mut n = LinkedList::new();
+        assert_eq!(n.iter().rev().next(), None);
+        n.push_front(4);
+        let mut it = n.iter().rev();
+        assert_eq!(it.size_hint(), (1, Some(1)));
+        assert_eq!(it.next().unwrap(), &4);
+        assert_eq!(it.size_hint(), (0, Some(0)));
+        assert_eq!(it.next(), None);
+    }
+
+    #[test]
+    fn test_mut_iter() {
+        let m = generate_test();
+        let mut len = m.len();
+        for (i, elt) in m.iter_mut().enumerate() {
+            assert_eq!(i as i32, *elt);
+            len -= 1;
+        }
+        assert_eq!(len, 0);
+        let mut n = LinkedList::new();
+        assert!(n.iter_mut().next().is_none());
+        n.push_front(4);
+        n.push_back(5);
+        let mut it = n.iter_mut();
+        assert_eq!(it.size_hint(), (2, Some(2)));
+        assert!(it.next().is_some());
+        assert!(it.next().is_some());
+        assert_eq!(it.size_hint(), (0, Some(0)));
+        assert!(it.next().is_none());
+    }
+
+    #[test]
+    fn test_iterator_mut_double_end() {
+        let mut n = LinkedList::new();
+        assert!(n.iter_mut().next_back().is_none());
+        n.push_front(4);
+        n.push_front(5);
+        n.push_front(6);
+        let mut it = n.iter_mut();
+        assert_eq!(it.size_hint(), (3, Some(3)));
+        assert_eq!(*it.next().unwrap(), 6);
+        assert_eq!(it.size_hint(), (2, Some(2)));
+        assert_eq!(*it.next_back().unwrap(), 4);
+        assert_eq!(it.size_hint(), (1, Some(1)));
+        assert_eq!(*it.next_back().unwrap(), 5);
+        assert!(it.next_back().is_none());
+        assert!(it.next().is_none());
+    }
+
+    #[test]
+    fn test_eq() {
+        let mut n: LinkedList<u8> = list_from(&[]);
+        let mut m = list_from(&[]);
+        assert!(n == m);
+        n.push_front(1);
+        assert!(n != m);
+        m.push_back(1);
+        assert!(n == m);
+
+        let n = list_from(&[2, 3, 4]);
+        let m = list_from(&[1, 2, 3]);
+        assert!(n != m);
+    }
+
+    #[test]
+    fn test_ord() {
+        let n = list_from(&[]);
+        let m = list_from(&[1, 2, 3]);
+        assert!(n < m);
+        assert!(m > n);
+        assert!(n <= n);
+        assert!(n >= n);
+    }
+
+    #[test]
+    fn test_ord_nan() {
+        let nan = 0.0f64 / 0.0;
+        let n = list_from(&[nan]);
+        let m = list_from(&[nan]);
+        assert!(!(n < m));
+        assert!(!(n > m));
+        assert!(!(n <= m));
+        assert!(!(n >= m));
+
+        let n = list_from(&[nan]);
+        let one = list_from(&[1.0f64]);
+        assert!(!(n < one));
+        assert!(!(n > one));
+        assert!(!(n <= one));
+        assert!(!(n >= one));
+
+        let u = list_from(&[1.0f64, 2.0, nan]);
+        let v = list_from(&[1.0f64, 2.0, 3.0]);
+        assert!(!(u < v));
+        assert!(!(u > v));
+        assert!(!(u <= v));
+        assert!(!(u >= v));
+
+        let s = list_from(&[1.0f64, 2.0, 4.0, 2.0]);
+        let t = list_from(&[1.0f64, 2.0, 3.0, 2.0]);
+        assert!(!(s < t));
+        assert!(s > one);
+        assert!(!(s <= one));
+        assert!(s >= one);
+    }
+
+    #[test]
+    fn test_debug() {
+        let list: LinkedList<i32> = (0..10).collect();
+        assert_eq!(format!("{:?}", list), "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]");
+
+        let list: LinkedList<&str> = vec!["just", "one", "test", "more"]
+            .iter()
+            .copied()
+            .collect();
+        assert_eq!(format!("{:?}", list), r#"["just", "one", "test", "more"]"#);
+    }
+
+    #[test]
+    fn test_hashmap() {
+        // Check that HashMap works with this as a key
+
+        let list1: LinkedList<i32> = (0..10).collect();
+        let list2: LinkedList<i32> = (1..11).collect();
+        let mut map = std::collections::HashMap::new();
+
+        assert_eq!(map.insert(list1.clone(), "list1"), None);
+        assert_eq!(map.insert(list2.clone(), "list2"), None);
+
+        assert_eq!(map.len(), 2);
+
+        assert_eq!(map.get(&list1), Some(&"list1"));
+        assert_eq!(map.get(&list2), Some(&"list2"));
+
+        assert_eq!(map.remove(&list1), Some("list1"));
+        assert_eq!(map.remove(&list2), Some("list2"));
+
+        assert!(map.is_empty());
     }
 }
